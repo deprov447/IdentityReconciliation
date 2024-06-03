364bbe7b8b5b19bff07d1efdb9b9c697ba585f4c
deprov447 <57551996+deprov447@users.noreply.github.com>
2024-06-03 20:40:53 +0530

    builds response in required format
    - @TODO: try reducing DB calls

--- src/identifyHandler.js
+++ src/identifyHandler.js
@@ -1,9 +1,9 @@
+const { Op } = require("sequelize")
 const db = require("./db/api")
 
 async function identifyHandler(req, res) {
-    const { phoneNumber, email } = req.body
-
     await db.syncModel()
+    const { phoneNumber, email } = req.body
 
     const phoneNumberMatches = await db.findAll({ where: { phoneNumber } })
     const emailMatches = await db.findAll({ where: { email } })
@@ -16,11 +16,33 @@ async function identifyHandler(req, res) {
         const primaryPhoneNumber = phoneNumberMatches.find((ele) => (ele.linkPrecedence == 'primary'))
         const primaryEmail = emailMatches.find((ele) => (ele.linkPrecedence == 'primary'))
 
-        if (primaryEmail.id != primaryPhoneNumber.id)
+        if (primaryEmail && primaryPhoneNumber && primaryEmail.id != primaryPhoneNumber.id)
             await db.updateLatestToSecondary(primaryPhoneNumber, primaryEmail)
     }
 
-    res.send(req.body)
+    const rows = await db.findAll({
+        where: {
+            [Op.or]: [
+                { phoneNumber },
+                { email }
+            ]
+        }
+    })
+
+    let primaryRow = rows.find((ele) => ele.linkPrecedence == 'primary')
+    if (primaryRow == null)
+        primaryRow = await db.findAll({ where: { id: rows[0].linkedId } })[0]
+
+    let secondaryRows = await db.findAll({ where: { linkedId: primaryRow.id } })
+
+    const resJSON = {
+        primaryContatctId: primaryRow.id,
+        emails: [...new Set([primaryRow.email, ...secondaryRows.map(row => row.email)].filter(ele => ele))],
+        phoneNumbers: [...new Set([primaryRow.phoneNumber, ...secondaryRows.map(row => row.phoneNumber)].filter(ele => ele))],
+        secondaryContactIds: secondaryRows.map(row => row.id)
+    }
+
+    res.send({ contact: resJSON })
 }
 
 module.exports = { identifyHandler }
