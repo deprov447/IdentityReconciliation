d5134dd1f99964532fa9d07f2172a05892288580
deprov447 <57551996+deprov447@users.noreply.github.com>
2024-06-03 08:49:25 +0530

    Adds logic for `/identify` route
    - If neither phoneNumber nor email exists in the DB, creates a primary row
    - If one of either phoneNumber or email already exists in DB, creates a secondary row and links it back to primary one
    - If both phoneNumber and email exist in DB, but are in different rows, links them

--- src/identifyHandler.js
+++ src/identifyHandler.js
@@ -1,4 +1,26 @@
-function identifyHandler(req, res) {
+const db = require("./db/api")
+
+async function identifyHandler(req, res) {
+    const { phoneNumber, email } = req.body
+
+    await db.syncModel()
+
+    const phoneNumberMatches = await db.findAll({ where: { phoneNumber } })
+    const emailMatches = await db.findAll({ where: { email } })
+
+    if (phoneNumberMatches.length == 0 && emailMatches.length == 0)
+        await db.createPrimary(phoneNumber, email)
+    else if (phoneNumberMatches.length == 0 || emailMatches.length == 0)
+        await db.createSecondary(phoneNumber, email)
+    else {
+        const primaryPhoneNumber = phoneNumberMatches.find((ele) => (ele.linkPrecedence == 'primary'))
+        const primaryEmail = emailMatches.find((ele) => (ele.linkPrecedence == 'primary'))
+
+        if (primaryEmail.id != primaryPhoneNumber.id)
+            await db.updateLatestToSecondary(primaryPhoneNumber, primaryEmail)
+    }
+
+    res.send(req.body)
 }
 
 module.exports = { identifyHandler }
